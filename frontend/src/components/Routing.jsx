80465d05b5de27a879cee1899c35544cf67949d7
Alouane Mohamed Amine <113364976+AlouaneMohamedAmine@users.noreply.github.com>
2024-03-08 22:31:22 +0100

    front end begun

---
+++ frontend/src/components/Routing.jsx
@@ -0,0 +1,41 @@
+import {BrowserRouter, Routes, Route } from "react-router-dom";
+ 
+// Imported
+import Home from "../pages/Home";
+import LibraryPage from "../pages/LibraryPage";
+import ProfilePage from "../pages/ProfilePage";
+import WebtoonPage from "../pages/WebtoonPage";
+import ChapterPage from "../pages/ChapterPage";
+import About from "../pages/About";
+import ErrorPage from "../pages/ErrorPage";
+import Browse from "../pages/Browse";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+
+
+const allRoutes = [
+      { path: "/", component: <Home />},
+      { path: "/browse", component: <Browse />},
+      { path: "/webtoon", component: <WebtoonPage />},
+      { path: "/webtoonname/chapter", component: <ChapterPage />},
+      { path: "/profile/library", component: <LibraryPage />},
+      { path: "/profile", component: <ProfilePage />},
+      { path: "/about", component: <About />},
+      { path: "/*", component: <ErrorPage />},
+      { path: "/login", component: <LoginPage />},
+      { path: "/register", component: <RegisterPage />},
+    ];
+
+function Routing() {
+  return (
+      <BrowserRouter>
+      <Routes>
+        {allRoutes.map((route, i) => (
+          <Route key={i} path={route.path} element={route.component} />
+        ))}
+      </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default Routing ;
